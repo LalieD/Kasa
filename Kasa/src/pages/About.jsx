13fbb4d43676a6068d3bc95364c5da51d445f92c
Lalie Deceuninck <d.lalie@orange.fr>
2024-05-02 16:06:05 +0200

    add react router and add folders and files

---
+++ Kasa/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <p>Fiabilité</p>
+            <p>Respect</p>
+            <p>Service</p>
+            <p>Sécurité</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ Kasa/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound () {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a href="/">Retourner sur la page d'accueil</a>
+        </div>
+    );
+}
+
+export default NotFound;
